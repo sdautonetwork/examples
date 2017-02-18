7dd88c0f8c77a6b2aad2b5d2e589df47ca8a0feb
sdautonetwork <raymondzosa@gmail.com>
2017-02-18 12:06:39 -0800

    Configuring form
    Working on form

--- mail.php
+++ mail.php
@@ -3,14 +3,11 @@ $name = $_POST['name'];
 $email = $_POST['email'];
 $phone = $_POST['phone'];
 $call = $_POST['call'];
-$website = $_POST['website'];
-$priority = $_POST['priority'];
-$type = $_POST['type'];
 $message = $_POST['message'];
-$formcontent=" From: $name \n Phone: $phone \n Call Back: $call \n Website: $website \n Priority: $priority \n Type: $type \n Message: $message";
+$formcontent=" From: $name \n Phone: $phone \n Call Back: $call \n Message: $message";
 $recipient = "sales@sandiegoautonetwork.com";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!" . " -" . "<a href='form.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
+echo "Thank You!" . " -" . "<a href='contact.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
 ?>
